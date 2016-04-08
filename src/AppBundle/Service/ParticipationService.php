49adf1f52e711185740da092f8a6019b2dbbccfc
yanickj <jyanick@gmail.com>
2016-04-07 23:46:24 -0400

    comments and shuffle

--- src/AppBundle/Service/ParticipationService.php
+++ src/AppBundle/Service/ParticipationService.php
@@ -6,12 +6,24 @@ use Doctrine\ORM\EntityManager;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 use AppBundle\Entity\Project;
 
+/**
+ * Class ParticipationService
+ *
+ * A service for determining if the logged in user is a participant.
+ *
+ * @package AppBundle\Service
+ */
 class ParticipationService
 {
     /**
      * @var EntityManager
      */
     protected $participants;
+
+    /**
+     * @var mixed
+     */
+    protected $user;
 
     /**
      * AppExtension constructor.
@@ -21,15 +33,17 @@ class ParticipationService
     public function __construct(EntityManager $em, TokenStorage $context)
     {
         $this->participants = $em->getRepository('AppBundle:Participant');
-        $this->context = $context;
+        $this->user = $context->getToken()->getUser();
     }
 
     /**
+     * Returns true if the logged in user is participating in an project/campaign.
+     *
      * @param Project $project
      */
     public function isParticipant(Project $project)
     {
-        $participation = $this->participants->findOneBy(['project' => $project, 'name' => $this->context->getToken()->getUser()->getUsername()]);
+        $participation = $this->participants->findOneBy(['project' => $project, 'name' => $this->user->getUsername()]);
 
         return !is_null($participation);
     }
